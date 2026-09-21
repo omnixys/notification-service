ae9d59cee583baedb5803627a5e4015f06ec933c
omnixys-01 <133212199+Caleb-Script@users.noreply.github.com>
2026-09-21 10:56:37 +0200

    update(deps): update omnixys packages

--- src/config/env.ts
+++ src/config/env.ts
@@ -170,7 +170,7 @@ export const env = {
   ENCRYPTION_KEY: getEnv('ENCRYPTION_KEY', '', { required: true }),
 
   DEFAULT_TENANT_ID: getEnv('DEFAULT_TENANT_ID', '', { required: true }),
-  EVENT_TENANT_MAP: parseEventTenantMap(getEnv('EVENT_TENANT_MAP', '')),
+  EVENT_TENANT_MAP: parseEventTenantMap(getEnv('EVENT_TENANT_MAP', '', {required: true})),
 
   KEYCLOAK_HEALTH_URL: getEnv('KEYCLOAK_HEALTH_URL', '', { required: true }),
   TEMPO_HEALTH_URL: getEnv('TEMPO_HEALTH_URL', '', { required: true }),
